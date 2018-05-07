a449d6bc490f79990d43f2bd304a5d1b520b249c
Blake Swaidner <blake.swaidner@gmail.com>
2018-05-07 16:44:43 -0400

    Initial Commit

---
+++ AwayGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwayGame
+//
+//  Created by Blake Swaidner on 5/7/18.
+//  Copyright © 2018 AwayGame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'AwayGame' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AwayGame
+  pod 'Firebase/Core'
+  pod 'Firebase/Firestore'
+  pod 'Alamofire', '~> 4.5'
+  pod 'Kingfisher', '~> 4.0'
+  pod 'ObjectMapper', '~> 3.1'
+  pod 'AlamofireObjectMapper', '~> 5.0'  
+
+end
