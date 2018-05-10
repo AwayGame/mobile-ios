402526fb284c299e5c61f32f3a60fd00430d61cd
Blake Swaidner <blake.swaidner@gmail.com>
2018-05-10 11:18:17 -0400

    Task/InitialSetup/AG-001 #1 Completed initial setup for HomeTableViewController

--- AwayGame/View/Home/CreateTripCell.swift
+++ AwayGame/View/Home/CreateTripCell.swift
@@ -9,17 +9,33 @@
 import Kingfisher
 import UIKit
 
+protocol CreateTripDelegate: class {
+    func didCreatNewTrip()
+}
+
 class CreateTripCell: UITableViewCell {
 
     static let identifier = "CreateTripCell"
     
+    @IBOutlet weak var createTripLabel: UILabel!
     
+    weak var delegate: CreateTripDelegate?
     
     // MARK: - Initialization
     
     override func awakeFromNib() {
         self.selectionStyle = .none
-        
     }
     
+    func configureCell() {
+        createTripLabel.text = "Create a new trip"
+        createTripLabel.font = Theme.Font.h1
+        createTripLabel.textColor = Theme.Color.black
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func addTripTapped(_ sender: Any) {
+        delegate?.didCreatNewTrip()
+    }
 }
---
+++ AwayGame/View/Home/SavedTripsCell.swift
@@ -0,0 +1,33 @@
+//
+//  SavedTripsCell.swift
+//  AwayGame
+//
+//  Created by Blake Swaidner on 5/7/18.
+//  Copyright © 2018 AwayGame. All rights reserved.
+//
+
+import UIKit
+
+class SavedTripsCell: UITableViewCell {
+
+    // MARK: - Initialization
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+    }
+    
+    func configureCell(itenerary: Itenerary, indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else if indexPath.row == 1 {
+            
+        } else {
+            
+        }
+    }
+
+    
+
+
+}
