0ac12435adffc67d811779c0529a357cf02f1987
Blake Swaidner <blake.swaidner@gmail.com>
2018-07-10 23:02:02 -0400

    Task/StatusBar/AG-028 #28 Changed status bar to white

--- AwayGame/AppDelegate.swift
+++ AwayGame/AppDelegate.swift
@@ -15,8 +15,6 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-   // KingfisherManager.shared.cache.pathExtension = "jpg"
     
     // TODO: POST RELEASE TODO LIST
     /*
@@ -47,6 +45,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+        
         FirebaseApp.configure()
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         Twitter.sharedInstance().start(withConsumerKey: "PdYhw4Wp0TwWhWt3p8qQkGxW1", consumerSecret: "vRC2yHmkxpAGfCinYMGUz1VnOpsfJspHAcTkwP6wARprfYth6J")
